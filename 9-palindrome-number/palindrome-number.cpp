a4507493b727208a98604192e2e910b92ac0f574
ayush-tech05 <vishwaayush05@gmail.com>
2025-11-13 00:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (26.29%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0){
+            return false;
+
+        }
+        int og = x;
+        long rev = 0;
+        while(x!=0){
+            int num = x%10;
+            rev = rev *10 +num;
+            x/=10;
+
+        }
+        return og == rev;
+    }
+};
